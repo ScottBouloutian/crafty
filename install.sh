6368bbd20b6a667e775747dbd1f3792d8454430f
Scott Bouloutian <ScottBouloutian@gmail.com>
2019-08-11 20:05:29 -0400

    include additional terraform resources

--- install.sh
+++ install.sh
@@ -24,13 +24,17 @@ function config_installation() {
     msm_user_system=true
 }
 
+function install_cron() {
+    install_log "Installing MSM cron file"
+    sudo install -m0644 "$dl_dir/msm.cron" /etc/cron.d/msm || install_error "Couldn't install cron file"
+    sudo service crond reload
+}
+
 install_msm
 
 # Provision msm
 wget https://github.com/ScottBouloutian/thecraftmine/archive/master.zip -O /tmp/master.zip
 unzip /tmp/master.zip -d /tmp
 sudo cp /tmp/thecraftmine-master/msm/msm.conf /etc
-sudo cp /tmp/thecraftmine-master/msm/msm /etc/cron.d
 chmod +x /tmp/thecraftmine-master/msm/provision.sh
 sudo su -c /tmp/thecraftmine-master/msm/provision.sh minecraft
-sudo service crond restart
