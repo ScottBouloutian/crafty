37ce498697ef1cc21ce712196055935ff04e631f
Scott Bouloutian <ScottBouloutian@gmail.com>
2019-12-26 03:44:51 +0000

    install craftbukkit

--- msm/provision.sh
+++ msm/provision.sh
@@ -1,33 +1,24 @@
 #!/usr/bin/env bash
 
-export PATH=$PATH:/usr/local/bin
+sudo cp /tmp/msm.conf /etc
+sudo cp /tmp/.bashrc /opt/msm
+sudo su minecraft
+cd /opt/msm
 
 # Create a new server
 msm update --noinput
 msm server create thecraftmine
 msm jargroup create minecraft minecraft
 msm jargroup create minecraft-snapshot minecraft-snapshot
-msm thecraftmine jar minecraft-snapshot
-cp /tmp/thecraftmine-master/msm/server.properties /opt/msm/servers/thecraftmine
-cp /tmp/thecraftmine-master/msm/eula.txt /opt/msm/servers/thecraftmine
-cp /tmp/thecraftmine-master/msm/white-list.txt /opt/msm/servers/thecraftmine
+msm thecraftmine jar minecraft
+cp /tmp/server.properties /opt/msm/servers/thecraftmine
+cp /tmp/eula.txt /opt/msm/servers/thecraftmine
+cp /tmp/white-list.txt /opt/msm/servers/thecraftmine
 mkdir /dev/shm/msm
-
-# Install forge
-if [ ! -d "/opt/msm/jars/forge" ]; then
-    msm jargroup create forge https://s3.amazonaws.com/scottbouloutian-dev/cdn/forge-1.13.2-25.0.102.jar
-    msm jargroup getlatest forge
-    wget https://s3.amazonaws.com/scottbouloutian-dev/cdn/forge-1.13.2-25.0.102.zip -O /tmp/forge.zip
-    unzip /tmp/forge.zip -d /opt/msm/jars/forge
-else
-    msm jargroup changeurl forge https://s3.amazonaws.com/scottbouloutian-dev/cdn/forge-1.13.2-25.0.102.jar
-fi
 
 # Install mods
 mkdir -p /opt/msm/servers/thecraftmine/plugins
-wget https://dev.bukkit.org/projects/essentials/files/latest -O /opt/msm/servers/thecraftmine/plugins
-wget https://scottbouloutian-dev.s3.amazonaws.com/cdn/thecraftmine-datapacks.zip -O /tmp/datapacks.zip
-unzip /tmp/datapacks.zip -d /opt/msm/servers/thecraftmine/datapacks
+wget https://dev.bukkit.org/projects/worldedit/files/latest -O /opt/msm/servers/thecraftmine/plugins
 
 # Start the services
 msm thecraftmine start
